09e3271c8c881a51dcd85e1dc3cf9330a1705fbe
Junbeom Park <ephesi4ns623@gmail.com>
2024-10-26 21:01:43 +0900

    setting: fix site.config.js

--- site.config.js
+++ site.config.js
@@ -1,13 +1,12 @@
 const CONFIG = {
   // profile setting (required)
   profile: {
-    name: "morethanmin",
+    name: "Junbeom Park",
     image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
     role: "frontend developer",
-    bio: "I develop everything using node.",
-    email: "morethanmin.dev@gmail.com",
-    linkedin: "morethanmin",
-    github: "morethanmin",
+    bio: "I love developing.",
+    email: "ephesi4ns623@gmail.com",
+    github: "treephesians",
     instagram: "",
   },
   projects: [
@@ -18,15 +17,15 @@ const CONFIG = {
   ],
   // blog setting (required)
   blog: {
-    title: "morethan-log",
-    description: "welcome to morethan-log!",
+    title: "jblog",
+    description: "welcome to jblog!",
     scheme: "dark", // 'light' | 'dark' | 'system'
   },
 
   // CONFIG configration (required)
-  link: "https://morethan-log.vercel.app",
-  since: 2022, // If leave this empty, current year will be used.
-  lang: "en-US", // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES', 'ko-KR']
+  link: "https://blog-seven-woad-68.vercel.app/",
+  since: 2024, // If leave this empty, current year will be used.
+  lang: ["en-US", 'ko-KR'], // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES', 'ko-KR']
   ogImageGenerateURL: "https://og-image-korean.vercel.app", // The link to generate OG image, don't end with a slash
 
   // notion configuration (required)
